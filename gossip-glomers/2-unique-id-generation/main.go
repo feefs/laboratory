32653a30b638e9c4ef5d1fbd056ec0304ae8750a
feefs <feefs@berkeley.edu>
2024-10-13 00:27:28 -0700

    rewrite unique id server

--- gossip-glomers/2-unique-id-generation/main.go
+++ gossip-glomers/2-unique-id-generation/main.go
@@ -1,53 +1,42 @@
 package main
 
 import (
-	"encoding/json"
 	"strconv"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type Body struct {
+type GenerateRespBody struct {
 	maelstrom.MessageBody
-	Id int64 `json:"id"`
+	Id int `json:"id"`
 }
 
 func main() {
 	node := maelstrom.NewNode()
 
-	ids := make(chan int64) // Unbuffered channel
-
+	ids := make(chan int) // unbuffered channel
+	generating := false
 	node.Handle("init", func(msg maelstrom.Message) error {
+		if generating {
+			return nil
+		}
 		intIdStr := node.ID()[1:]
-		intId, err := strconv.ParseInt(intIdStr, 10, 64)
+		intId, err := strconv.Atoi(intIdStr)
 		if err != nil {
 			return err
 		}
-
-		// send first int64 to the ids channel in a new goroutine so this function doesn't block
 		go func() {
-			ids <- intId
+			for {
+				ids <- intId
+				intId += len(node.NodeIDs())
+			}
 		}()
-
+		generating = true
 		return node.Reply(msg, maelstrom.MessageBody{Type: "init_ok"})
 	})
 
 	node.Handle("generate", func(msg maelstrom.Message) error {
-		body := &Body{}
-		if err := json.Unmarshal(msg.Body, body); err != nil {
-			return err
-		}
-
-		id := <-ids
-		// defer sending the next int64 to the ids channel so this function doesn't block
-		defer func() {
-			ids <- id + int64(len(node.NodeIDs()))
-		}()
-
-		body.Id = id
-		body.Type = "generate_ok"
-
-		return node.Reply(msg, body)
+		return node.Reply(msg, &GenerateRespBody{maelstrom.MessageBody{Type: "generate_ok"}, <-ids})
 	})
 
 	if err := node.Run(); err != nil {
