6b211bb959a2c2f4f370a7a9696b086f536b4b3e
feefs <feefs@berkeley.edu>
2024-10-13 00:43:15 -0700

    remove unnecessary goroutine

--- gossip-glomers/4-grow-only-counter/server/add.go
+++ gossip-glomers/4-grow-only-counter/server/add.go
@@ -18,36 +18,25 @@ func (s *server) AddHandler(msg maelstrom.Message) error {
 		return err
 	}
 
-	errChan := make(chan error)
-	go func() {
-		var doneErr error
-		for {
-			ctx, cancel := context.WithTimeout(context.Background(), rpcTimeout)
-			defer cancel()
-			currentValue, err := s.kv.ReadInt(ctx, s.node.ID())
-			nextValue := reqBody.Delta
-			if err == nil {
-				nextValue += currentValue
-			} else if maelstrom.ErrorCode(err) != maelstrom.KeyDoesNotExist {
-				doneErr = err
-				break
-			}
-
-			ctx, cancel = context.WithTimeout(context.Background(), rpcTimeout)
-			defer cancel()
-			err = s.kv.CompareAndSwap(ctx, s.node.ID(), currentValue, nextValue, true)
-			if err == nil {
-				break
-			} else if maelstrom.ErrorCode(err) != maelstrom.PreconditionFailed {
-				doneErr = err
-				break
-			}
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), rpcTimeout)
+		defer cancel()
+		currentValue, err := s.kv.ReadInt(ctx, s.node.ID())
+		nextValue := reqBody.Delta
+		if err == nil {
+			nextValue += currentValue
+		} else if maelstrom.ErrorCode(err) != maelstrom.KeyDoesNotExist {
+			return err
 		}
-		errChan <- doneErr
-	}()
 
-	if err := <-errChan; err != nil {
-		return err
+		ctx, cancel = context.WithTimeout(context.Background(), rpcTimeout)
+		defer cancel()
+		err = s.kv.CompareAndSwap(ctx, s.node.ID(), currentValue, nextValue, true)
+		if err == nil {
+			break
+		} else if maelstrom.ErrorCode(err) != maelstrom.PreconditionFailed {
+			return err
+		}
 	}
 
 	return s.node.Reply(msg, &maelstrom.MessageBody{Type: "add_ok"})
