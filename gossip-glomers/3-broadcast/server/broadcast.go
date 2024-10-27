244db88d447df9ab4490a52f5cb5f9b21b78fe7d
feefs <feefs@berkeley.edu>
2024-10-27 00:20:17 -0700

    finish multi broadcast maelstrom challenge rewrite

--- gossip-glomers/3-broadcast/server/broadcast.go
+++ gossip-glomers/3-broadcast/server/broadcast.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"errors"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -12,12 +13,30 @@ type BroadcastReqBody struct {
 }
 
 func (s *server) BroadcastHandler(msg maelstrom.Message) error {
+	if len(msg.Src) == 0 {
+		return errors.New("empty caller type")
+	}
+
 	reqBody := &BroadcastReqBody{}
 	if err := json.Unmarshal(msg.Body, reqBody); err != nil {
 		return err
 	}
 
 	s.messagesChan <- reqBody.Message
+
+	if msg.Src[0] == 'n' {
+		return nil
+	}
+
+	for _, id := range s.node.NodeIDs() {
+		if id == s.node.ID() {
+			continue
+		}
+		s.node.Send(id, &BroadcastReqBody{
+			MessageBody: maelstrom.MessageBody{Type: "broadcast"},
+			Message:     reqBody.Message,
+		})
+	}
 
 	return s.node.Reply(msg, &maelstrom.MessageBody{Type: "broadcast_ok"})
 }
