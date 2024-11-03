aeb79aebd657c00b679592c4cc7a5cf3ecd7fede
feefs <feefs@berkeley.edu>
2024-11-03 00:05:05 -0700

    finish fault tolerant broadcast challenge rewrite

--- gossip-glomers/3-broadcast/server/broadcast.go
+++ gossip-glomers/3-broadcast/server/broadcast.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -24,18 +26,25 @@ func (s *server) BroadcastHandler(msg maelstrom.Message) error {
 
 	s.messagesChan <- reqBody.Message
 
-	if msg.Src[0] == 'n' {
-		return nil
-	}
-
-	for _, id := range s.node.NodeIDs() {
-		if id == s.node.ID() {
-			continue
+	if msg.Src[0] == 'c' {
+		for _, id := range s.node.NodeIDs() {
+			if id == s.node.ID() {
+				continue
+			}
+			go func() {
+				for {
+					ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+					defer cancel()
+					_, err := s.node.SyncRPC(ctx, id, &BroadcastReqBody{
+						MessageBody: maelstrom.MessageBody{Type: "broadcast"},
+						Message:     reqBody.Message,
+					})
+					if err == nil {
+						break
+					}
+				}
+			}()
 		}
-		s.node.Send(id, &BroadcastReqBody{
-			MessageBody: maelstrom.MessageBody{Type: "broadcast"},
-			Message:     reqBody.Message,
-		})
 	}
 
 	return s.node.Reply(msg, &maelstrom.MessageBody{Type: "broadcast_ok"})
