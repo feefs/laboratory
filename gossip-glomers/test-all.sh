3e2a96a749ff01462f492ca4a834900dcb012394
feefs <feefs@berkeley.edu>
2024-11-09 20:24:25 -0800

    fix cleanup functions and simplify logging for test-all.sh

--- gossip-glomers/test-all.sh
+++ gossip-glomers/test-all.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
+set -o pipefail
 
 output_path=/tmp/maelstrom-test-all-output-$(date +"%H-%M-%S")
 echo -e "\033[0;34m🌀🌀🌀 tail -f $output_path to see output 🌀🌀🌀\033[0m"
 
-cleanup() {
+prompt_cleanup() {
+  read -p "Delete $output_path? (y/n) [Default - y]: " answer
+  if [[ "$answer" != "n" ]]; then
+    echo "rm $output_path"
+    rm $output_path
+  fi
+}
+
+sigint_cleanup() {
   echo "Received SIGINT, deleting $output_path" 
   echo "rm $output_path"
   rm $output_path
+  exit 1
 }
-trap cleanup INT
+trap sigint_cleanup INT
 
 for dir in */; do
   dir=$(basename "$dir")
-  echo -e "\033[0;34m🌀🌀🌀 Testing $dir with Maelstrom... 🌀🌀🌀\033[0m"
-  output=$(make -C "$dir" test 2>&1 | tee $output_path)
+  echo -e "\033[0;34mTesting $dir...\033[0m"
+  output=$(make -C "$dir" test 2>&1 | tee -a $output_path)
   status=$?
   if [ "$status" -ne 0 ]; then
-    echo "$output"
-    echo -e "\033[0;31m🌀🌀🌀 Tests for $dir failed! 🌀🌀🌀\033[0m"
+    echo -e "\033[0;31mTests for $dir failed!\033[0m"
+    prompt_cleanup
     exit 1
   fi
-  echo -e "\033[0;32m🌀🌀🌀 Tests for $dir passed! 🌀🌀🌀\033[0m"
+  echo -e "\033[0;32mTests for $dir passed!\033[0m"
 done
 
-read -p "Delete $output_path? (y/n) [Default - y]: " answer
-if [[ "$answer" != "n" ]]; then
-  echo "rm $output_path"
-  rm $output_path
-fi
+echo -e "\033[0;34m🌀🌀🌀 All tests passed! 🌀🌀🌀\033[0m"
+prompt_cleanup
