5eebd6b26a21d2b65a2e5a1a77b9002abc653b5f
feefs <feefs@berkeley.edu>
2024-11-10 23:26:33 -0800

    delete old efficient broadcast ii implementation

--- gossip-glomers/3e-efficient-broadcast-ii/server/topology.go
+++
@@ -1,9 +0,0 @@
-package server
-
-import (
-	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
-)
-
-func (s *Server) TopologyHandler(msg maelstrom.Message) (err error) {
-	return s.Node.Reply(msg, &maelstrom.MessageBody{Type: "topology_ok"})
-}
