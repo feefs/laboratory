94355709c72fb090516f0af1b6793cbd9a806496
feefs <feefs@berkeley.edu>
2024-12-01 15:07:54 -0800

    use uuidV7 for unique id server

--- gossip-glomers/2-unique-id-generation/main.go
+++ gossip-glomers/2-unique-id-generation/main.go
@@ -1,34 +1,62 @@
 package main
 
 import (
+	"math/big"
+	"math/rand/v2"
 	"strconv"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 type GenerateRespBody struct {
 	maelstrom.MessageBody
-	Id int `json:"id"`
+	Id string `json:"id"`
+}
+
+func uuidV7(id int64) string {
+	unixTsMs := big.NewInt(time.Now().UnixMilli())
+	unixTsMs = unixTsMs.Lsh(unixTsMs, 80)
+
+	ver := big.NewInt(7)
+	ver = ver.Lsh(ver, 76)
+
+	randA := big.NewInt(id)
+	randA = randA.Lsh(randA, 64)
+
+	variant := big.NewInt(2)
+	variant = variant.Lsh(variant, 62)
+
+	randB := big.NewInt(rand.Int64())
+	randB = randB.And(randB, big.NewInt(0x3FFFFFFFFFFFFFFF))
+
+	result := new(big.Int)
+	result.Or(result, unixTsMs)
+	result.Or(result, ver)
+	result.Or(result, randA)
+	result.Or(result, variant)
+	result.Or(result, randB)
+
+	return result.Text(16)
 }
 
 func main() {
 	node := maelstrom.NewNode()
 
-	ids := make(chan int) // unbuffered channel
+	ids := make(chan string) // unbuffered channel
 	generating := false
 	node.Handle("init", func(msg maelstrom.Message) error {
 		if generating {
 			return nil
 		}
 		intIdStr := node.ID()[1:]
-		intId, err := strconv.Atoi(intIdStr)
+		intId, err := strconv.ParseInt(intIdStr, 10, 64)
 		if err != nil {
 			return err
 		}
 		go func() {
 			for {
-				ids <- intId
-				intId += len(node.NodeIDs())
+				ids <- uuidV7(intId)
 			}
 		}()
 		generating = true
